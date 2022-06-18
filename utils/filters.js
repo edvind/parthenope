abc84c40fcce5a9bc4a8fdcb2db2803b5eb6f5cd
edvind <edvind@domedagis.org>
2022-06-18 18:01:36 +0200

    plugin filter info

--- utils/filters.js
+++ utils/filters.js
@@ -1,3 +1,11 @@
+// @11ty/eleventy-plugin-rss supplied nunjucks filters
+//
+// getNewestCollectionItemDate
+// dateToRfc3339      2022-06-18T07:20:50.52Z
+// dateToRfc822       Sat, 18 Jun 2022 09:20:50 CEST
+// absoluteUrl 
+// htmlToAbsoluteUrls
+
 module.exports = {
   // Example filter
   filter: function (str) {
