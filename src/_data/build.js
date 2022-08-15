213c93ddd4bff3265f8694c2acc0539748cf66a0
edvind <edvind@domedagis.org>
2022-08-15 21:40:19 +0200

    prod/dev/stage urls

--- src/_data/build.js
+++ src/_data/build.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv').config();
 
+function buildUrl( env ) {
+  let buildUrl;
+  if ( env == "production" ) {
+    buildUrl = process.env.PRODUCTION_URL;
+  } else if ( env == "development" ) {
+    buildUrl = process.env.DEVELOPMENT_URL;
+  } else if ( env == "staging" ) {
+    buildUrl = process.env.STAGING_URL;
+  }
+  return buildUrl || "http://localhost:8080";
+}
+
 module.exports = {
   env: process.env.ELEVENTY_ENV,
   lang: process.env.DEFAULT_LANG || "en",
   timestamp: new Date(),
-  url: process.env.URL || "http://localhost:8080"
+  url: buildUrl( process.env.ELEVENTY_ENV )
 }
